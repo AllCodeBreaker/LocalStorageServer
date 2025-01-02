2beb29d2f7df8e41f387d3b650fd1703431ec533
allcodebreaker <allwin2436@gmail.com>
2025-01-02 20:27:24 +0530

    Started using reactjs

---
+++ lcc/src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/Header"
+
+function App(){
+  return(
+    <>
+      <Header />
+      <main>
+        <p>Hello, World!</p>
+      </main>
+    </>
+   
+    )
+}
+
+
+
+export default App;
---
+++ lcc/src/components/Header.js
@@ -0,0 +1,12 @@
+import "./header.css"
+
+const Header = ()=>{
+    return (
+        <header className="header">
+            <p>Local Storage Server</p>
+
+        </header>
+    )
+}
+
+export default Header;
